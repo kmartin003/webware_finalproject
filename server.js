624b1db1dc68e8ccb080998ab74e61e797170730
Kevin <kpmartin@wpi.edu>
2015-12-17 00:00:50 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var path = require('path');
 
 var app = express();
@@ -7,6 +8,16 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/')));
 app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
+
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
 });
 
 app.listen(port, function() {
