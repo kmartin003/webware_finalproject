47ba572d4ea2236414e990a89cafadeb6845e4ae
Kevin <kpmartin@wpi.edu>
2015-12-17 01:46:59 -0500

    maybe works?

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',function(req,res){
   res.sendfile("index.html");
@@ -16,8 +17,10 @@ app.get('/',function(req,res){
 
 app.post('/login',function(req,res){
   var user_name=req.body.user;
-  console.log("User name = "+user_name+", password is "+password);
-  res.end("yes");
+  var score=req.body.score;
+  console.log("User name = "+user_name);
+  res.send = 'sdasdas';
+  res.end("done");
 });
 
 app.listen(port, function() {
